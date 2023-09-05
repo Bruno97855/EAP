56f6b439fb8ba8537b255305be4787ae3aecd0dc
LuckFerrs <143883491+LuckFerrs@users.noreply.github.com>
2023-09-05 07:57:21 -0300

    atualizacoes necessarias

---
+++ EAP/Pages/CriacaoPersonagem/Passo2Atrib.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EAP.Pages.CriacaoPersonagem
+{
+    public class Passo2AtribModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
