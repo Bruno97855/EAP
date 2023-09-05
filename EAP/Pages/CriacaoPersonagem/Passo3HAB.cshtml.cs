7c5a053c848c8424b36fc95610f21b4d22955b5b
LuckFerrs <143883491+LuckFerrs@users.noreply.github.com>
2023-09-05 07:57:21 -0300

    atualizacoes necessarias

---
+++ EAP/Pages/CriacaoPersonagem/Passo3HAB.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EAP.Pages.CriacaoPersonagem
+{
+    public class Passo3HABModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
