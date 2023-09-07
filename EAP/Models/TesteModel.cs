3d421b6bd870bee71b9aa24b7f83a470974aa865
Bruno Rossete <bruno.rossete@grupoimg.com.br>
2023-09-07 00:20:22 -0300

    Criação de adronização

---
+++ EAP/Models/TesteModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EAP.Models
+{
+    public class TesteModel
+    {
+        public string? TextBox { get; set; }
+        public List<SelectListItem> DropDownList { get; set; }
+    }
+}
