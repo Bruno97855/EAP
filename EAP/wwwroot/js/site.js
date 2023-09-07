af9dd2f6e3acc296bc73ae7a2031d41377861901
Bruno Rossete <bruno.rossete@grupoimg.com.br>
2023-09-07 18:39:21 -0300

    Ajustes no dropdown

--- EAP/wwwroot/js/site.js
+++ EAP/wwwroot/js/site.js
@@ -6,3 +6,40 @@
         navbar.style.right = "-250px";
     }
 }
+
+$(document).ready(function () {
+    var toggleButton = $(".custom-dropdown-toggle");
+    var dropdownMenu = $(".custom-dropdown-menu");
+
+    toggleButton.click(function () {
+        dropdownMenu.toggle();
+    });
+
+    dropdownMenu.on("click", "li", function () {
+        var selectedOption = $(this);
+        var selectedOptionValue = selectedOption.data("value");
+        var selectedOptionText = selectedOption.text();
+
+        // Atualize o texto e o valor do botão de alternância com o texto e o valor da opção selecionada
+        toggleButton.text(selectedOptionText);
+        toggleButton.attr("value", selectedOptionValue);
+
+        // Feche o menu suspenso
+        dropdownMenu.hide();
+    });
+
+    $(document).click(function (e) {
+        if (
+            !toggleButton.is(e.target) &&
+            !dropdownMenu.is(e.target) &&
+            dropdownMenu.has(e.target).length === 0
+        ) {
+            dropdownMenu.hide();
+        }
+    });
+});
+
+
+
+
+
